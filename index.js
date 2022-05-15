273f27ae88baa3b8f4c2c86910a1f89bfa6a077a
Michael Kamau <kamau.karitu@gmail.com>
2022-05-15 13:25:48 +0300

    Vue Loader  - Install and configure vue loader  - Install vue style loader

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
 import './src/styles/sass/styles.scss';
+import {app} from './src/Vuejs/main.js'
---
+++ src/Vuejs/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <hello-world msg="Hello from my message"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./Components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
